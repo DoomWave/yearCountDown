3fcb7d8cc34ed2bbdcf4ce53847e3e8b3bab53cc
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2026-01-16 21:29:25 -0800

    counter finished.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dayEl = document.getElementById("day");
 const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
-const secondEl = document.getElementById("seconds");
+const secondEl = document.getElementById("second");
 
 const newYearTime = new Date("Jan 1, 2027 00:00:00").getTime()
 
@@ -17,5 +17,12 @@ function updateCountdown(){
     const day = hour * 24;
 
     const d = Math.floor(gap/day);
-    
+    const h = Math.floor((gap % day) / hour);
+    const m = Math.floor((gap % hour) / minute)
+    const s = Math.floor((gap % minute) / second);
+    dayEl.innerText = d
+    hourEl.innerText = h
+    minuteEl.innerText = m;
+    secondEl.innerText = s;
+    setTimeout(updateCountdown, 1000);
 };
